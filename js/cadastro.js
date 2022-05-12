96faf21696eb7c840d1c7b011dc09ed59bbd7aa1
Samuel Martins <101484995+Samuel-GMartins@users.noreply.github.com>
2022-05-12 20:27:29 -0300

    atualização cad e perfil

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,23 +1,29 @@
 var formcad = document.forms.formCadastro
 
 document.querySelector("#btcad").onclick = function () {
-    if (formcad.nomeCadastro.value == "") {
+    if (formcad.nome.value == "") {
         alert("Preencha o nome!")
 
-    } else if (formcad.emailCadastro.value == "") {
+    } else if (formcad.email.value == "") {
         alert("Preencha o E-mail!")
     }
-    else if (formcad.celCadastro.value == "") {
+    else if (formcad.telefone.value == "") {
         alert("Digite um telefone para Contato")
     }
-    else if (formcad.paswCadastro.value == "") {
+    else if (formcad.senha.value == "") {
         alert("Escolha uma Senha Forte")
     }
-    else if (formcad.paswCadastroConf.value !== paswCadastro.value || formcad.paswCadastroConf.value == "") {
+    else if (formcad.senhaF.value !== senha.value || formcad.senhaF.value == "") {
         alert("As Senhas não conferem. Tente Novamente")
     }
 
     else{
         formcad.submit();
+        localStorage.nomeUser = formCadastro.nome.value
+        localStorage.emailUser = formCadastro.email.value
+        localStorage.telefoneUser = formCadastro.telefone.value
+        localStorage.senhaUser = formCadastro.senha.value
+
+
     }
 }
--- js/perfil-usuario-json.js
+++ js/perfil-usuario-json.js
@@ -1,6 +1,6 @@
 
 // Escopos var let const
-let cliente = '{"nome": "Samuel Martins", "imagem": "fotoCliente.jpeg", "idade":"23","email":"dev.gmartins@gmail.com", "preferencia": "Não", "dataInicio": " 28/03/2022", "tipoAssinante":"Administrador", "valor": 250.75}'
+let cliente = '{"Nome": "Samuel Martins", "imagem": "fotoCliente.jpeg", "idade":"23","email":"dev.gmartins@gmail.com", "preferencia": "Terror", "dataInicio": " 28/03/2022", "tipoAssinante":"Administrador", "valor": 250.75}'
 
 let clienteJson = JSON.parse(cliente)
 
@@ -10,19 +10,24 @@ let imagem = document.querySelector("#imagem")
 let conteudo = document.querySelector("#conteudo")
 let titulo = document.querySelector("#titulo")
 let nome = document.querySelector("#nome")
-let idade = document.querySelector("#idade")
 let email = document.querySelector("#email")
+let telefone = document.querySelector("#telefone")
+let idade = document.querySelector("#idade")
 let preferencia = document.querySelector("#preferencia")
 let dataInicio = document.querySelector("#dataInicio")
 let tipoAssinante = document.querySelector("#tipoAssinante")
 let valor = document.querySelector("#valor")
 
+console.log(nome)
+
+
 titulo.innerHTML = "Página do Usuário"
 imagem.innerHTML = '<img style="border-radius:50%" src="images/'  + clienteJson.imagem + '"width="300"/>'
-nome.innerHTML = "<b>Nome:</b> " + clienteJson.nome
+nome.innerHTML = "<b>Nome:</b> " + localStorage.nomeUser
 idade.innerHTML = "<b>Idade: </b>" + clienteJson.idade
-email.innerHTML = "<b>E-mail:</b> " + clienteJson.email
-preferencia.innerHTML = "<b>Possui Preferências: </b>" + clienteJson.preferencia
+email.innerHTML = "<b>E-mail:</b> " + localStorage.emailUser
+telefone.innerHTML = "<b>Telefone:</b> " + localStorage.telefoneUser
+preferencia.innerHTML = "<b>Preferências: </b>" + clienteJson.preferencia
 dataInicio.innerHTML = "<b>Data De Início: </b>" + clienteJson.dataInicio
 tipoAssinante.innerHTML = "<b>Tipo De Assinatura: </b>" + clienteJson.tipoAssinante
 valor.innerHTML = "<b>Valor: R$ </b>" + clienteJson.valor
