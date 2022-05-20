353f2255388fc8700b46aa1237da1d9eafcacade
Samuel Martins <101484995+Samuel-GMartins@users.noreply.github.com>
2022-05-20 16:52:12 -0300

    att

--- js/contato.js
+++ js/contato.js
@@ -1,19 +1,28 @@
 var formc = document.forms.formContato
+var idRandon = parseInt(Math.random() * 90000) + 10000
+var idRandonKey = parseInt(Math.random() * 10) + 100
 
-document.querySelector("#btcontato").onclick = function () {
+
+
+document.querySelector("#btContato").onclick = function () {
     if (formc.nomecontato.value == "") {
         alert("Preencha o nome!")
 
     } else if (formc.emailcontato.value == "") {
         alert("Preencha o E-mail!")
-    }
-    else if (formc.assuntocontato.value == "") {
+    } else if (formc.assuntocontato.value == "") {
         alert("Digite o Assunto do Comentário!")
-    }
-    else if (formc.mensagemcontato.value == "") {
+    } else if (formc.mensagemcontato.value == "") {
         alert("Escreva Um Comentário")
-    }
-    else{
+
+    } else if (formc.chamado.checked) {
+        localStorage.setItem('idChamado' + idRandonKey, 'chamado-' + idRandon)
+        localStorage.setItem('nomeChamado' + idRandonKey, formc.nomecontato.value)
+        localStorage.setItem('emailChamado' + idRandonKey, formc.emailcontato.value)
+        localStorage.setItem('assuntoChamado' + idRandonKey, formc.assuntocontato.value)
+        
+        formc.submit();
+    } else {
         formc.submit();
     }
 }
