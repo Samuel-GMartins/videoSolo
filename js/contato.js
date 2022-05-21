d23d7e9ede1aa8899f8e717935e5b5d68a9dfb6d
Samuel Martins <101484995+Samuel-GMartins@users.noreply.github.com>
2022-05-21 20:29:38 -0300

    att

--- js/contato.js
+++ js/contato.js
@@ -1,28 +1,33 @@
 var formc = document.forms.formContato
 var idRandon = parseInt(Math.random() * 90000) + 10000
-var idRandonKey = parseInt(Math.random() * 10) + 100
-
-
 
 document.querySelector("#btContato").onclick = function () {
     if (formc.nomecontato.value == "") {
-        alert("Preencha o nome!")
 
-    } else if (formc.emailcontato.value == "") {
+        alert("Preencha o nome!")
+    
+        } else if (formc.emailcontato.value == "") {
         alert("Preencha o E-mail!")
+
+    }
+    
+    else if (formc.telcontato.value == "") {
+        alert("Digite um telefone!")
+
     } else if (formc.assuntocontato.value == "") {
         alert("Digite o Assunto do Comentário!")
+        
     } else if (formc.mensagemcontato.value == "") {
         alert("Escreva Um Comentário")
 
     } else if (formc.chamado.checked) {
-        localStorage.setItem('idChamado' + idRandonKey, 'chamado-' + idRandon)
-        localStorage.setItem('nomeChamado' + idRandonKey, formc.nomecontato.value)
-        localStorage.setItem('emailChamado' + idRandonKey, formc.emailcontato.value)
-        localStorage.setItem('assuntoChamado' + idRandonKey, formc.assuntocontato.value)
+
+        let arrPeoples = [formc.nomecontato.value,formc.emailcontato.value,formc.telcontato.value,formc.assuntocontato.value]
+        localStorage.setItem('chamado-' + idRandon,arrPeoples)
         
         formc.submit();
     } else {
         formc.submit();
     }
-}
+}         
+
