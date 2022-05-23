6def7570b0a1f5e1b35225a9f63329c74b797f1b
Samuel Martins <101484995+Samuel-GMartins@users.noreply.github.com>
2022-05-23 20:18:34 -0300

    att

--- js/carrinho.js
+++ js/carrinho.js
@@ -13,7 +13,6 @@ somatotal = preco1 + preco2 + preco3
 let pro1 = document.querySelector('#pro1').innerHTML = valorpro1
 let pro2 = document.querySelector('#pro2').innerHTML = valorpro2
 let pro3 = document.querySelector('#pro3').innerHTML = valorpro3
-
 let pre1 = document.querySelector('#pre1').innerHTML = 'R$ ' + parseFloat(preco1).toFixed(2)
 let pre2 = document.querySelector('#pre2').innerHTML = 'R$ ' + parseFloat(preco2).toFixed(2)
 let pre3 = document.querySelector('#pre3').innerHTML = 'R$ ' + parseFloat(preco3).toFixed(2)
@@ -42,6 +41,11 @@ function calculaValor2() {
     document.querySelector('#tot').innerHTML = 'R$ ' + total.toFixed(2);
 }
 
+
+
+tela1 = document.querySelector('#tela1')
+
+
 document.querySelector("#botao").onclick = function () {
 
     if (formDesconto.cupom.value == "CUPOM123") {
@@ -54,4 +58,42 @@ document.querySelector("#botao").onclick = function () {
         formDesconto.submit()
 
     }
+}
+
+
+
+
+document.querySelector("#btCont").onclick = function () {telas()}
+
+telas = function () {
+
+    var pre2 = document.querySelector('#pre2')
+    var pre3 = document.querySelector('#pre3')
+
+    if (tela1.value == 2) {
+        var porc7 = parseFloat((preco1 * 7) / 100)
+        preco1 = (preco1 + porc7).toFixed(2)
+        var pre1 = document.querySelector('#pre1').innerHTML = 'R$ ' + parseFloat(preco1).toFixed(2)
+
+    } else if (tela1.value == 3) {
+        var porc7 = parseFloat((preco1 * 14) / 100)
+
+        preco1 = (preco1 + (porc7 * 2)).toFixed(2)
+        var pre1 = document.querySelector('#pre1').innerHTML = 'R$ ' + parseFloat(preco1).toFixed(2)
+
+    } else if (tela1.value == 4) {
+        var porc7 = parseFloat((preco1 * 21) / 100)
+        preco1 = (preco1 + (porc7 * 3)).toFixed(2)
+        var pre1 = document.querySelector('#pre1').innerHTML = 'R$ ' + parseFloat(preco1).toFixed(2)
+
+    } else if (tela1.value == 5) {
+        var porc7 = parseFloat((preco1 * 28) / 100)
+        preco1 = (preco1 + (porc7 * 4)).toFixed(2)
+        var pre1 = document.querySelector('#pre1').innerHTML = 'R$ ' + parseFloat(preco1).toFixed(2)
+
+    } else {
+        let pre1 = document.querySelector('#pre1').innerHTML = 'R$ ' + parseFloat(preco1).toFixed(2)
+
+    }
+
 }
