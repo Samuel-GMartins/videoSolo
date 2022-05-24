6fb5d88d8c98d7b95df846e4f127fd91f240cc1a
Samuel Martins <101484995+Samuel-GMartins@users.noreply.github.com>
2022-05-23 21:37:29 -0300

    att

--- js/carrinho.js
+++ js/carrinho.js
@@ -2,9 +2,15 @@ valorpro1 = 1
 valorpro2 = 2
 valorpro3 = 1
 
-preco1 = 12.50
-preco2 = 15
-preco3 = 11
+prepro1= 12.50
+prepro2= 15
+prepro3= 11
+
+
+
+preco1 = prepro1 * valorpro1
+preco2 = prepro2 * valorpro2
+preco3 = prepro3 * valorpro3
 
 quantidaDeProdutos = valorpro1 + valorpro2 + valorpro3
 somatotal = preco1 + preco2 + preco3
@@ -13,9 +19,9 @@ somatotal = preco1 + preco2 + preco3
 let pro1 = document.querySelector('#pro1').innerHTML = valorpro1
 let pro2 = document.querySelector('#pro2').innerHTML = valorpro2
 let pro3 = document.querySelector('#pro3').innerHTML = valorpro3
-let pre1 = document.querySelector('#pre1').innerHTML = 'R$ ' + parseFloat(preco1).toFixed(2)
-let pre2 = document.querySelector('#pre2').innerHTML = 'R$ ' + parseFloat(preco2).toFixed(2)
-let pre3 = document.querySelector('#pre3').innerHTML = 'R$ ' + parseFloat(preco3).toFixed(2)
+let pre1 = document.querySelector('#pre1').innerHTML = telas1()
+let pre2 = document.querySelector('#pre2').innerHTML = telas2()
+let pre3 = document.querySelector('#pre3').innerHTML = telas3()
 
 
 let totprod = document.querySelector('#totprod').innerHTML = quantidaDeProdutos + ' Produtos'
@@ -43,9 +49,6 @@ function calculaValor2() {
 
 
 
-tela1 = document.querySelector('#tela1')
-
-
 document.querySelector("#botao").onclick = function () {
 
     if (formDesconto.cupom.value == "CUPOM123") {
@@ -63,13 +66,12 @@ document.querySelector("#botao").onclick = function () {
 
 
 
-document.querySelector("#btCont").onclick = function () {telas()}
+document.querySelector("#btCont").onclick = function () {
+    telas1(), telas2(), telas3()
 
-telas = function () {
+}
 
-    var pre2 = document.querySelector('#pre2')
-    var pre3 = document.querySelector('#pre3')
-
+function telas1() {
     if (tela1.value == 2) {
         var porc7 = parseFloat((preco1 * 7) / 100)
         preco1 = (preco1 + porc7).toFixed(2)
@@ -77,7 +79,6 @@ telas = function () {
 
     } else if (tela1.value == 3) {
         var porc7 = parseFloat((preco1 * 14) / 100)
-
         preco1 = (preco1 + (porc7 * 2)).toFixed(2)
         var pre1 = document.querySelector('#pre1').innerHTML = 'R$ ' + parseFloat(preco1).toFixed(2)
 
@@ -91,9 +92,113 @@ telas = function () {
         preco1 = (preco1 + (porc7 * 4)).toFixed(2)
         var pre1 = document.querySelector('#pre1').innerHTML = 'R$ ' + parseFloat(preco1).toFixed(2)
 
-    } else {
+    } else if (tela1.value == 1) {
         let pre1 = document.querySelector('#pre1').innerHTML = 'R$ ' + parseFloat(preco1).toFixed(2)
 
     }
+}
 
+function telas2() {
+    if (tela2.value == 2) {
+        var porc7 = parseFloat((preco2 * 7) / 100)
+        preco2 = (preco2 + porc7).toFixed(2)
+        var pre2 = document.querySelector('#pre2').innerHTML = 'R$ ' + parseFloat(preco2).toFixed(2)
+
+    } else if (tela2.value == 3) {
+        var porc7 = parseFloat((preco2 * 14) / 100)
+        preco2 = (preco2 + (porc7 * 2)).toFixed(2)
+        var pre2 = document.querySelector('#pre2').innerHTML = 'R$ ' + parseFloat(preco2).toFixed(2)
+
+    } else if (tela2.value == 4) {
+        var porc7 = parseFloat((preco2 * 21) / 100)
+        preco2 = (preco2 + (porc7 * 3)).toFixed(2)
+        var pre2 = document.querySelector('#pre2').innerHTML = 'R$ ' + parseFloat(preco2).toFixed(2)
+
+    } else if (tela2.value == 5) {
+        var porc7 = parseFloat((preco2 * 28) / 100)
+        preco2 = (preco2 + (porc7 * 4)).toFixed(2)
+        var pre2 = document.querySelector('#pre2').innerHTML = 'R$ ' + parseFloat(preco2).toFixed(2)
+
+    } else if (tela2.value == 1) {
+        let pre2 = document.querySelector('#pre2').innerHTML = 'R$ ' + parseFloat(preco2).toFixed(2)
+
+    }
+
+}
+
+function telas3() {
+    if (tela3.value == 2) {
+        var porc7 = parseFloat((preco3 * 7) / 100)
+        preco3 = (preco3 + porc7).toFixed(2)
+        var pre3 = document.querySelector('#pre3').innerHTML = 'R$ ' + parseFloat(preco3).toFixed(2)
+
+    } else if (tela3.value == 3) {
+        var porc7 = parseFloat((preco3 * 14) / 100)
+        preco3 = (preco3 + (porc7 * 2)).toFixed(2)
+        var pre3 = document.querySelector('#pre3').innerHTML = 'R$ ' + parseFloat(preco3).toFixed(2)
+
+    } else if (tela3.value == 4) {
+        var porc7 = parseFloat((preco3 * 21) / 100)
+        preco3 = (preco3 + (porc7 * 3)).toFixed(2)
+        var pre3 = document.querySelector('#pre3').innerHTML = 'R$ ' + parseFloat(preco3).toFixed(2)
+
+    } else if (tela3.value == 5) {
+        var porc7 = parseFloat((preco3 * 28) / 100)
+        preco3 = (preco3 + (porc7 * 4)).toFixed(2)
+        var pre3 = document.querySelector('#pre3').innerHTML = 'R$ ' + parseFloat(preco3).toFixed(2)
+
+    } else if (tela3.value == 1) {
+        let pre3 = document.querySelector('#pre3').innerHTML = 'R$ ' + parseFloat(preco3).toFixed(2)
+
+    }
+
+}
+
+
+
+document.querySelector("#excluir1").onclick = function () {
+    var x;
+    var resposta = confirm("Você Tem Certeza Disso?");
+    if (resposta==true){
+        x="você pressionou OK!";
+        linha1.remove()
+  } else {
+      null
+  }
+}
+
+
+
+document.querySelector("#excluir2").onclick = function () {
+    var x;
+    var resposta = confirm("Você Tem Certeza Disso?");
+    if (resposta==true){
+        x="você pressionou OK!";
+        linha2.remove()
+  } else {
+    null
+}
+}
+
+
+document.querySelector("#excluir3").onclick = function () {
+    var x;
+    var resposta = confirm("Você Tem Certeza Disso?");
+    if (resposta==true){
+        x="você pressionou OK!";
+        linha3.remove()
+  } else {
+    null
+}
+}
+
+document.querySelector("#limparCar").onclick = function () {
+    var x;
+    var resposta = confirm("Você Tem Certeza Disso?");
+    if (resposta==true){
+        carrinhoProd.remove()
+
+  } else {
+    null
+}
 }
