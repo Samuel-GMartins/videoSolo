4e23ef5c0eecb97efd32e4c2a5ed8c8b99225b65
Samuel Martins <101484995+Samuel-GMartins@users.noreply.github.com>
2022-05-30 16:34:19 -0300

    Att

--- js/index.js
+++ js/index.js
@@ -33,9 +33,13 @@ function imprimeTela() {
     let favorito1 = localStorage.getItem()
 }
 
-favorito1 = document.querySelector('#favorito1').src = localStorage.getItem('Doutor Estranho')
-favorito2 = document.querySelector('#favorito2').src = localStorage.getItem('A Freira')
-favorito3 = document.querySelector('#favorito3').src = localStorage.getItem('Em Defesa de Cristo')
 
 
-console.log(localStorage.getItem())
+function imprimirEmTela (favorito1,favorito2,favorito3){ 
+    for 
+    favorito1 = document.querySelector('#favorito1').src = localStorage.getItem()
+    favorito2 = document.querySelector('#favorito2').src = localStorage.getItem()
+    favorito3 = document.querySelector('#favorito3').src = localStorage.getItem()
+}
+
+imprimirEmTela()
